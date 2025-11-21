1c26182355db5a1a9838e973cb55f2404c14a98b
MaSeungHyun <seunghyun.ma@grapicar.com>
2025-11-21 13:34:00 +0900

    feat: 공통 Button 컴포넌트 구현

--- apps/web/src/components/Button.tsx
+++ apps/web/src/components/Button.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import { cn } from "../utils/style";
 
 export interface ButtonProps
@@ -6,24 +6,23 @@ export interface ButtonProps
   children: React.ReactNode;
 }
 
-export const Button = ({
-  children,
-  className = "",
-  ...props
-}: ButtonProps): React.ReactNode => {
-  return (
-    <button
-      className={cn(
-        "bg-black-700 text-gray-100 h-fit min-h-8 w-fit min-w-24 rounded-md border px-6 py-1",
-        !props.disabled &&
-          "hover:bg-black-500 border-cyan-300 hover:cursor-pointer hover:border-cyan-200",
-        props.disabled &&
-          "hover:bg-black-700 border-cyan-400 text-gray-300 hover:cursor-default",
-        className
-      )}
-      {...props}
-    >
-      {children}
-    </button>
-  );
-};
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ children, className = "", ...props }, ref) => {
+    return (
+      <button
+        className={cn(
+          "bg-black text-gray-100 h-fit min-h-8 w-fit min-w-24 rounded-md border px-6 py-1",
+          !props.disabled &&
+            "hover:bg-black-500 border-cyan-300 hover:cursor-pointer hover:border-cyan-200",
+          props.disabled &&
+            "hover:bg-black-700 border-cyan-400 text-gray-300 hover:cursor-default",
+          className
+        )}
+        {...props}
+        ref={ref}
+      >
+        {children}
+      </button>
+    );
+  }
+);
