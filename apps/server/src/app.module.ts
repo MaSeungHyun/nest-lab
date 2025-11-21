169dc75b63ef1f9e54ba585bc41cbdbad4744367
MaSeungHyun <seunghyun.ma@grapicar.com>
2025-11-21 15:16:56 +0900

    fix: 예시로 사용하고 삭제한 파일 import 제거하지 않아 서버가 실행되지 않던 오류 해결

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DogController } from './dog/dog.controller';
-import { DogService } from './dog/dog.service';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, DogController, AuthController],
-  providers: [AppService, DogService, AuthService],
+  controllers: [AppController, AuthController],
+  providers: [AppService, AuthService],
 })
 export class AppModule {}
