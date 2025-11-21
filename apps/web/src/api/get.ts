3cfd2ddc8c1d09f0091b2fe3ad70d24a28f45188
MaSeungHyun <seunghyun.ma@grapicar.com>
2025-11-21 13:36:11 +0900

    chore: 사용하지 않는 불필요한 파일 제거

--- apps/web/src/api/get.ts
+++
@@ -1,7 +0,0 @@
-export const getDogs = async () => {
-  const res = await fetch("http://localhost:3000/dogs");
-  if (!res.ok) {
-    throw new Error("Network response was not ok");
-  }
-  return res.json();
-};
